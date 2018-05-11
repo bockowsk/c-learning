ef7ab66cfa5436e20d64f60d5e61033ffde72f91
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2018-05-11 22:57:08 +0200

    po dlugich walkach dziala

--- Structs/z4run.c
+++ Structs/z4run.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "z3.h"
 
 int main(int argc, char *argv[]) {
-   extern struct s1 kwartal;
-   int ile=funkcja1(2,kwartal);
+   extern struct s1 kwartal[];
+   int ile=funkcja1(3,kwartal);
    printf("ILE: %i\n",ile);
    puts("koniec");
 }
--- Structs/z5.c
+++ Structs/z5.c
@@ -1,10 +1,11 @@
 #include "z3.h"
 int funkcja1(int ktory, struct s1 struktura[]) {
     int j=0;
-    int suma_dni;
-    int suma_numerow;
+    int suma_dni=0;
+    int suma_numerow=0;
     for (j;j<ktory;j++) {
-        suma_dni=(*(struktura+j)).days;
+        
+        suma_dni+=(*(struktura+j)).days;
         //suma_dni+=(*(struktura[j])).days;
         //suma_dni+=(struktura[j])->days;
         //suma_numerow=+(struktura+j)->month;
