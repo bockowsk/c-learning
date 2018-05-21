ecd49c354fb3d2c8599257a953ee7bb84e63a57e
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2018-05-21 17:04:13 +0200

    refactoring na fscanf

--- r13z4.c
+++ r13z4.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
+    FILE *plik;
     int licznik=0;
     float suma=0.0f;
     float pobrana=0.0f;
@@ -11,19 +12,14 @@ int main(int argc, char *argv[]) {
         exit(2);
     } 
     else if (argc == 2) {
-        FILE *plik;
         plik=fopen(argv[1],"r");
-        while (fscanf(plik,"%f",&pobrana)==1) {
-            licznik++;
-            suma+=pobrana;
-        }
     }
     else {
-        while (scanf("%f",&pobrana)==1) {
-            licznik++;
-            suma+=pobrana;
-        }          
-
+        plik=stdin;
+    }
+    while (fscanf(plik,"%f",&pobrana)==1) {
+        licznik++;
+        suma+=pobrana;
     }
     printf("suma floatow: %f\n",suma);
 }
